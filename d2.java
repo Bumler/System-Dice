b153791bb5827f3940d7fa1b89120113855d391c
Bumler <hbulmer@callutheran.edu>
2016-09-06 23:33:31 -0700

    Implementning multiple dice types

---
+++ d2.java
@@ -0,0 +1,7 @@
+package com.example.isstech.systemdice;
+
+/**
+ * Created by Bumbum on 9/6/2016.
+ */
+public class d2 {
+}
--- die.java
+++ die.java
@@ -1,54 +1,11 @@
 package com.example.isstech.systemdice;
 
-public class die {
-    int face = 0;
-
-    public die(){
-        roll();
-    }
-
-    public int getFace(){
-        return face;
-    }
-
-    //generates a die with a face 1-6
-    public void roll (){
-        face = 1 + (int)(Math.random()*6);
-    }
-
-    public int getDamageExplode(){
-        if (face == 1){
-            return 0;
-        }
-        else if (face == 6){
-            die explo = new die();
-            return 2 + explo.getDamage();
-        }
-        else return 1;
-    }
-
-    public int getDamageDoubleExplode(){
-        if (face == 1){
-            return 0;
-        }
-        else if (face == 6){
-            die explo = new die();
-            return 2 + explo.getDamage();
-        }
-        else if (face == 5){
-            die explo = new die();
-            return 1 + explo.getDamage();
-        }
-        else return 1;
-    }
-
-    public int getDamage(){
-        if (face == 1){
-            return 0;
-        }
-        else if (face == 6){
-            return 2;
-        }
-        else return 1;
-    }
+/**
+ * Created by Bumbum on 9/6/2016.
+ */
+public interface die {
+    public int getFace();
+    public int getDamageExplode();
+    public int getDamageDoubleExplode();
+    public int getDamage();
 }
